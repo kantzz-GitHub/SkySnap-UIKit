9990ee3ec86109222125813a3651ed2fab68037a
Shermukhammad Usmonov <codingblyatt@gmail.com>
2023-11-17 10:38:31 -0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Shermukhammad Usmonov on 2023-11-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
